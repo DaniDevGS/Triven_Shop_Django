8ce6af0dc950d6dc6e06cee52b9e545d44db4618
DaniDev <danieldavidsh8@gmail.com>
2025-11-21 18:29:57 -0400

    Testing 8

--- built.sh
+++ built.sh
@@ -2,9 +2,13 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# 1. Instalar dependencias
 pip install -r requirements.txt
 
+# 3. Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+# 2. **Ejecutar migraciones (CRÍTICO)**
 python manage.py migrate
 
+python manage.py loaddata products_data.json
